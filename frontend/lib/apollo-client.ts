cc730823b001162075a81e67cffdd4e8dd9a6d39
Nevyana Angelova <nevyana@scomot.com>
2025-03-04 12:59:14 +0700

    Add eslint and formatting

--- frontend/lib/apollo-client.ts
+++ frontend/lib/apollo-client.ts
@@ -2,9 +2,9 @@ import {ApolloClient, InMemoryCache, HttpLink} from '@apollo/client';
 
 const client = new ApolloClient({
     link: new HttpLink({
-        uri: 'http://localhost:8080/graphql',
+        uri: 'http://localhost:8080/graphql'
     }),
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache()
 });
 
 export default client;
--- frontend/lib/queries.ts
+++ frontend/lib/queries.ts
@@ -1,23 +1,23 @@
-import { gql } from '@apollo/client';
+import {gql} from '@apollo/client';
 
 export const GET_PROJECTS = gql`
-  query GetProjects {
-    projects {
-      id
-      name
+    query GetProjects {
+        projects {
+            id
+            name
+        }
     }
-  }
 `;
 
 export const GET_PROJECT_WITH_TASKS = gql`
-  query GetProjectWithTasks($id: ID!) {
-    project(id: $id) {
-      id
-      name
-      tasks {
-        id
-        name
-      }
+    query GetProjectWithTasks($id: ID!) {
+        project(id: $id) {
+            id
+            name
+            tasks {
+                id
+                name
+            }
+        }
     }
-  }
 `;
